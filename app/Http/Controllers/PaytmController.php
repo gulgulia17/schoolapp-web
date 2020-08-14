8229a4f9a14ec344153563b1ae5f8dc0214e58a7
gulgulia17 <gulgulia17@gmail.com>
2020-08-14 17:28:59 +0530

    Fixed Paytm Controller

--- app/Http/Controllers/PaytmController.php
+++ app/Http/Controllers/PaytmController.php
@@ -25,32 +25,35 @@ class PaytmController extends Controller
      */
     public function index(Request $request)
     {
-        // dd($request['_token']);
-        $amount = Plans::where('id', $request->subscription)->first();
-        if(empty($amount)){
-             return back()->with('error', 'Something Wrong');
-        }
-        $order = 'SchoolApp'.rand(111111, 999999).'@'.$request->subscription.date('d');
-        $data = [
-            "CUST_ID" => rand(111111, 999999),
-            "INDUSTRY_TYPE_ID" => "Retail",
-            "CHANNEL_ID" => 'WEB',
-            "MID" => PAYTM_MERCHANT_MID,
-            "WEBSITE" => PAYTM_MERCHANT_WEBSITE,
-            "CALLBACK_URL" => route('status'),
-            "MOBILE_NO" =>  $request->phone,
-            "EMAIL" =>  $request->email,
-        ];
-        $data['TXN_AMOUNT'] = $amount->amount;
-        $data['ORDER_ID'] = $order;
-        $data['CHECKSUMHASH'] = getChecksumFromArray($data, PAYTM_MERCHANT_KEY);
-         $response = $request->validate([
+        $response = $request->validate([
             "name" => 'required|string',
             "email" => 'required|email',
             "phone" => 'required',
             "subscription" => 'required|string',
         ]);
+
+        $amount = Plans::where('id', $request->subscription)->first();
+        if (empty($amount)) {
+            return back()->with('error', 'Something Wrong');
+        }
+        $order = 'SchoolApp' . rand(111111, 999999) . '@' . $request->subscription . date('d');
         $response['ORDERID'] = $order;
+        $data = [
+            'MID' => env('MID'),
+            'ORDER_ID' => $order,
+            'CUST_ID' => "CUST" . rand(10000, 99999999),
+            'INDUSTRY_TYPE_ID' => env('INDUSTRY_TYPE_ID'),
+            'CHANNEL_ID' => env('CHANNEL_ID'),
+            'TXN_AMOUNT' => $amount->amount,
+            'CALLBACK_URL' => route('status'),
+            'WEBSITE' => env('WEBSITE'),
+            'MSISDN' => $request->phone,
+            'EMAIL' => $request->email,
+            'VERIFIED_BY' => $request->email,
+            'IS_USER_VERIFIED' => 'YES',
+        ];
+        $data['CHECKSUMHASH'] = getChecksumFromArray($data, env('MK'));
+
         orderHistory::create($response);
         $this->payment($data);
     }
@@ -65,7 +68,7 @@ class PaytmController extends Controller
             "ORDERID"    => $request->ORDERID,
             "TXNID"      => $request->TXNID,
             "TXNAMOUNT"  => $request->TXNAMOUNT,
-            "PAYMENTMODE"=> $request->PAYMENTMODE,
+            "PAYMENTMODE" => $request->PAYMENTMODE,
             "TXNDATE"    => $request->TXNDATE,
             "STATUS"     => $request->STATUS,
             "RESPCODE"   => $request->RESPCODE,
@@ -76,35 +79,26 @@ class PaytmController extends Controller
         if ($request->RESPCODE == '01') {
             Mail::to($checkData->email)->send(new OrderPlaceMail($data));
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL,"http://127.0.0.1:8001/api/payment");
+            curl_setopt($ch, CURLOPT_URL, env('API_BACKEND'));
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($response));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $server_output = curl_exec($ch);
             dd($server_output);
-            curl_close ($ch);
+            curl_close($ch);
         }
         return view('order', compact('response'));
     }
 
     public function payment($data)
     {
-        // dd($data);
-        $url = PAYTM_TXN_URL;
-        $options = array(
-            'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'POST',
-                'content' => http_build_query($data)
-            )
-        );
-        
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-        if ($result === FALSE) {
-            print_r($result);
+        $d = '<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><style>*{margin:0;padding:0;box-sizing:border-box}body,html{margin:0;padding:0;height:100%;font-family:sans-serif}.loader-wrapper{height:100%;width:100%;padding-bottom:20px;position:absolute}.loader-box{position:absolute;text-align:center;background:#fff;min-width:200px;left:0;right:0;top:0;bottom:0;margin:auto;height:150px}.loader-box h3{font-size:24px;color:#000;margin:15px 0 12px 0}.loader-box img{max-width:80px}.loader-box p{font-size:17px;color:#666;line-height:24px;margin:0 auto;padding:0 15%}.spinner{width:70px;text-align:center;display:none}.spinner>div{width:10px;height:10px;background:#012b71;border-radius:100%;display:inline-block;-webkit-animation:sk-bouncedelay 1s infinite ease-in-out both;-ms-animation:sk-bouncedelay 1s infinite ease-in-out both;-moz-animation:sk-bouncedelay 1s infinite ease-in-out both;-o-animation:sk-bouncedelay 1s infinite ease-in-out both;animation:sk-bouncedelay 1s infinite ease-in-out both}.spinner .bounce1{-webkit-animation-delay:-.64s;animation-delay:-.64s}.spinner .bounce2{-webkit-animation-delay:-.48s;animation-delay:-.48s}.spinner .bounce3{-webkit-animation-delay:-.32s;animation-delay:-.32s}.spinner .bounce4{-webkit-animation-delay:-.16s;animation-delay:-.16s;background:#48baf5}.spinner .bounce5{background:#48baf5}@-ms-keyframes sk-bouncedelay{0%,100%,80%{-ms-transform:scale(.2)}40%{-ms-transform:scale(1)}}@-o-keyframes sk-bouncedelay{0%,100%,80%{-o-transform:scale(.2)}40%{-o-transform:scale(1)}}@-moz-keyframes sk-bouncedelay{0%,100%,80%{-moz-transform:scale(.2)}40%{-moz-transform:scale(1)}}@-webkit-keyframes sk-bouncedelay{0%,100%,80%{-webkit-transform:scale(.2)}40%{-webkit-transform:scale(1)}}@keyframes sk-bouncedelay{0%,100%,80%{-webkit-transform:scale(.2);transform:scale(.2)}40%{-webkit-transform:scale(1);transform:scale(1)}}@media all and (max-width:900px){.spinner{display:inline-block}.loader-img{display:none}}</style></head><body>';
+        $d .= '<form action="' . PAYTM_TXN_URL . '" method="POST">';
+        foreach ($data as $key => $value) {
+            $d .= "<input hidden type='text' name=" . $key . " id=" . $key . " value=" . $value . ">";
         }
-        echo $result;
-        // echo $server_output;
+        $d .= '</form><div class=' . "loader-wrapper" . '><div class=' . "loader-box" . '><div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div><div class="bounce4"></div><div class="bounce5"></div></div><img class="loader-img" src="https://staticgw1.paytm.in/common/images/loader.gif" alt="Loading..." /><h3>Processing</h3><p>Please do not press ' . "Back" . ' or ' . "Refresh" . ' button</p></div></div>';
+        $d .= '<script type="text/javascript">document.forms[0].submit();</script></body></html>';
+        echo $d;
     }
 }
--- app/PaytmKit/lib/config_paytm.php
+++ app/PaytmKit/lib/config_paytm.php
@@ -1,15 +1,8 @@
 <?php
-/*
-- Use PAYTM_ENVIRONMENT as 'PROD' if you wanted to do transaction in production environment else 'TEST' for doing transaction in testing environment.
-- Change the value of PAYTM_MERCHANT_KEY constant with details received from Paytm.
-- Change the value of PAYTM_MERCHANT_MID constant with details received from Paytm.
-- Change the value of PAYTM_MERCHANT_WEBSITE constant with details received from Paytm.
-- Above details will be different for testing and production environment.
-*/
 
 define('PAYTM_ENVIRONMENT', 'TEST'); // PROD
-define('PAYTM_MERCHANT_KEY', 'LLdC1vUj1rHkE_fi'); //Change this constant's value with Merchant key received from Paytm.
-define('PAYTM_MERCHANT_MID', 'jARKoK90225623891882'); //Change this constant's value with MID (Merchant ID) received from Paytm.
+define('PAYTM_MERCHANT_KEY', 'C2&aUH6SU%bfqwbn'); //Change this constant's value with Merchant key received from Paytm.
+define('PAYTM_MERCHANT_MID', 'VKQdgb54237262582028'); //Change this constant's value with MID (Merchant ID) received from Paytm.
 define('PAYTM_MERCHANT_WEBSITE', 'WEBSTAGING'); //Change this constant's value with Website name received from Paytm.
 
 $PAYTM_STATUS_QUERY_NEW_URL='https://securegw-stage.paytm.in/order/status';
